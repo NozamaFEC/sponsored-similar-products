04d0bea6d7caf22d4074c86e4e7c0ea1930a33f6
Emilio-miloCastaneda <57383306+Emilio-miloCastaneda@users.noreply.github.com>
2020-03-05 15:59:08 -0600

    corrected the static browser display to render on an express route function i.e. app.route('/bundle').

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const path = require('path');
 
 App.use(express.static(path.join(process.cwd(), '/Client-side/dist')))
 
+App.route('/bundle')
+    .get((req, res) => {
+        res.sendFile(path.join(process.cwd(), '/client/dist/bundle.js'))
+    })
+
 App.listen(4000, ()=>{
     console.log(`listening on port 4000`)
 });
